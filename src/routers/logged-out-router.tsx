095993c9f76ca5d274d768979a43389268061f56
BM <chane81@naver.com>
2021-05-04 20:06:22 +0900

    useForm 사용

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,15 +1,50 @@
 import React from 'react';
-import { isLoggedInVar } from '../apollo';
+import { useForm } from 'react-hook-form';
 
 export const LoggedOutRouter = () => {
-  const handleClick = () => {
-    isLoggedInVar(true);
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit = () => {
+    console.log(watch('email'));
+  };
+
+  const onInvalid = () => {
+    console.log('errors', errors);
+    console.log('cant create account');
   };
 
   return (
     <div>
       <h1>Logged Out</h1>
-      <button onClick={handleClick}>Click to login</button>
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div>
+          <input
+            {...register('email', {
+              required: 'email required',
+              pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/
+              // validate: (email: string) => email.includes('@gmail.com')
+            })}
+            name='email'
+            type='email'
+            placeholder='email'
+          />
+        </div>
+        <div>
+          <input
+            {...register('password', {
+              required: true
+            })}
+            type='password'
+            placeholder='password'
+          />
+        </div>
+        <button className='bg-yellow-300 text-white'>Submit</button>
+      </form>
     </div>
   );
 };
