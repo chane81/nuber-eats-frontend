87a2d775ca014d9cc3e6c9163260fd7f30f51e61
BM <chane81@naver.com>
2021-05-04 20:06:08 +0900

    코드 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { useQuery, useReactiveVar } from '@apollo/client';
-import gql from 'graphql-tag';
 import React from 'react';
+import { useReactiveVar } from '@apollo/client';
 import { isLoggedInVar } from './apollo';
 import { LoggedInRouter } from './routers/logged-in-router';
 import { LoggedOutRouter } from './routers/logged-out-router';
