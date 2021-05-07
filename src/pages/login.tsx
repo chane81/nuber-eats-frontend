0560921377a61fedbfebb577c3b759b89f98f24e
BM <chane81@naver.com>
2021-05-07 20:08:00 +0900

    react-hook-form 적용 벨리데이션 추가

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,22 +1,72 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+
+interface ILoginForm {
+  email?: string;
+  password?: string;
+}
 
 export const Login = () => {
+  const {
+    register,
+    getValues,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<ILoginForm>();
+
+  const onSubmit = () => {
+    console.log('submit');
+    console.log(getValues(), errors);
+  };
+
+  const onInvalid = () => {
+    console.log('errors', errors);
+    console.log('cant create account');
+  };
+
   return (
     <div className='h-screen flex items-center justify-center bg-gray-800'>
-      <div className='bg-gray-200 w-full max-w-lg py-10 rounded-lg text-center'>
+      <div className='bg-gray-200 w-full max-w-lg pt-10 pb-7 rounded-lg text-center'>
         <h3 className='text-2xl text-gray-800 font-mono'>Log In</h3>
-        <form className='flex flex-col mt-5 px-5'>
+        <form
+          onSubmit={handleSubmit(onSubmit, onInvalid)}
+          className='grid gap-3 mt-5 px-5'
+        >
           <input
+            {...register('email', {
+              required: 'Email is required'
+            })}
+            name='email'
+            type='email'
             placeholder='Email'
-            className='bg-gray-100 shadow-xl border-2 focus:outline-none focus:border-opacity-60 focus:border-blue-700 mb-3 py-3 px-5 rounded-lg'
+            className='input mb-3'
           />
+          {errors.email?.message && (
+            <span className='font-medium text-red-500'>
+              {errors.email?.message}
+            </span>
+          )}
           <input
+            {...register('password', {
+              required: 'Password is required',
+              minLength: 10
+            })}
+            name='password'
+            type='password'
             placeholder='Password'
-            className='bg-gray-100 shadow-xl border-2 focus:outline-none  focus:border-opacity-60 focus:border-blue-700 py-3 px-5 rounded-lg'
+            className='input'
           />
-          <button className='py-3 px-5 bg-gray-800 text-white mt-3 text-lg rounded-lg focus:outline-none hover:opacity-90'>
-            Log In
-          </button>
+          {errors.password?.message && (
+            <span className='font-medium text-red-500'>
+              {errors.password?.message}
+            </span>
+          )}
+          {errors.password?.type === 'minLength' && (
+            <span className='font-medium text-red-500'>
+              Password must be more than 10 chars.
+            </span>
+          )}
+          <button className='button mt-3'>Log In</button>
         </form>
       </div>
     </div>
