ca51ad5918391a5cd053a1bf570d7554cf1a08d9
chane81 <chane81@naver.com>
2021-05-23 00:39:04 +0900

    pagination 구현

--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -1,4 +1,5 @@
 import { gql, useQuery } from '@apollo/client';
+import { useState } from 'react';
 import { Category } from '../../components/category';
 import { Restaurant } from '../../components/restaurant';
 import {
@@ -40,17 +41,20 @@ const RESTAURANTS_QUERY = gql`
 `;
 
 export const Restaurants = () => {
+  const [page, setPage] = useState(1);
   const { data, loading, error } = useQuery<
     restaurantsPageQuery,
     restaurantsPageQueryVariables
   >(RESTAURANTS_QUERY, {
     variables: {
       input: {
-        page: 1,
+        page,
       },
     },
-    fetchPolicy: 'no-cache',
   });
+
+  const onNextPageClick = () => setPage((current) => current + 1);
+  const onPrevPageClick = () => setPage((current) => current - 1);
 
   return (
     <div>
@@ -62,7 +66,7 @@ export const Restaurants = () => {
         />
       </form>
       {!loading && (
-        <div className='max-w-screen-2xl mx-auto mt-8'>
+        <div className='max-w-screen-2xl pb-20 mx-auto mt-8'>
           <div className='flex justify-around max-w-lg mx-auto'>
             {data?.allCategories.categories?.map((category) => (
               <Category
@@ -82,6 +86,31 @@ export const Restaurants = () => {
                 categoryName={restaurant.category?.name}
               />
             ))}
+          </div>
+          <div className='grid grid-cols-3 text-center max-w-md items-center mx-auto mt-10'>
+            {page > 1 ? (
+              <button
+                onClick={onPrevPageClick}
+                className='focus:outline-none font-medium text-2xl'
+              >
+                &larr;
+              </button>
+            ) : (
+              <div />
+            )}
+            <span className='mx-5'>
+              Page {page} of {data?.restaurants.totalPages}
+            </span>
+            {page !== data?.restaurants.totalPages ? (
+              <button
+                onClick={onNextPageClick}
+                className='focus:outline-none font-medium text-2xl'
+              >
+                &rarr;
+              </button>
+            ) : (
+              <div />
+            )}
           </div>
         </div>
       )}
