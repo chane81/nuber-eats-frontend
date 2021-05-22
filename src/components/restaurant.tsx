5305f0aaa47e0c01d1cdc6a16e10bcf3f1f5e367
chane81 <chane81@naver.com>
2021-05-23 00:12:47 +0900

    restaurant 를 컴포넌트로 분리

---
+++ src/components/restaurant.tsx
@@ -0,0 +1,21 @@
+interface IProps {
+  id: string;
+  coverImg: string;
+  name: string;
+  categoryName?: string;
+}
+
+export const Restaurant: React.FC<IProps> = ({ coverImg, name, categoryName }) => {
+  return (
+    <div className='flex flex-col'>
+      <div
+        style={{ backgroundImage: `url(${coverImg})` }}
+        className='py-28 bg-cover bg-center mb-3'
+      />
+      <h3 className='text-xl'>{name}</h3>
+      <span className='border-t mt-1 py-2 text-xs opacity-50 border-gray-400'>
+        {categoryName}
+      </span>
+    </div>
+  );
+};
