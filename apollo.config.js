4ba9624218ebad3b2ef06ce3c43b2a1bb31331b4
chane81 <chane81@naver.com>
2021-05-25 02:24:37 +0900

    ts 와 tsx 파일 모두에 대해서 generate 할 수 있게 수정

--- apollo.config.js
+++ apollo.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   client: {
-    // tsx 가 확장자인 파일에서 query 를 검색
-    includes: ['./src/**/*.tsx'],
+    // tsx | ts 인 확장자인 파일에서 query 를 검색
+    includes: ['./src/**/*.{ts,tsx}'],
     // tag 명
     tagName: 'gql',
     // graphql backend setting
