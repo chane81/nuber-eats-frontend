34fb87846c040b4e3ff49670631d7392471df2b3
chane81 <chane81@naver.com>
2021-05-25 03:22:09 +0900

    search restaurant lazy query 추가

--- src/pages/client/search.tsx
+++ src/pages/client/search.tsx
@@ -1,5 +1,28 @@
+import { gql, useLazyQuery } from '@apollo/client';
 import { useEffect } from 'react';
+import { Helmet } from 'react-helmet-async';
+import { useHistory } from 'react-router-dom';
+import { RESTAURANT_FRAGMENT } from '../../fragments';
 import useUrlQuery from '../../hooks/useUrlQuery';
+import {
+  searchRestaurant,
+  searchRestaurantVariables,
+} from '../../__generated__/searchRestaurant';
+
+const SEARCH_RESTAURANT = gql`
+  query searchRestaurant($input: SearchRestaurantInput!) {
+    searchRestaurant(input: $input) {
+      ok
+      error
+      totalPages
+      totalResults
+      restaurants {
+        ...RestaurantParts
+      }
+    }
+  }
+  ${RESTAURANT_FRAGMENT}
+`;
 
 interface IQueryState {
   term: string;
@@ -7,9 +30,35 @@ interface IQueryState {
 
 export const Search = () => {
   const urlQuery = useUrlQuery<IQueryState>();
+  const history = useHistory();
+  const [callQuery, { loading, data, called }] =
+    useLazyQuery<searchRestaurant, searchRestaurantVariables>(SEARCH_RESTAURANT);
 
-  useEffect(() => {}, []);
-  console.log('urlquery', urlQuery.state, urlQuery.get('term'));
+  useEffect(() => {
+    const query = urlQuery.get('term');
 
-  return <h1>Search Page</h1>;
+    if (!query) {
+      return history.push('/');
+    }
+
+    callQuery({
+      variables: {
+        input: {
+          page: 1,
+          query,
+        },
+      },
+    });
+  }, []);
+
+  console.log(loading, data, called);
+
+  return (
+    <div>
+      <Helmet>
+        <title>Search | Nuber Eats</title>
+      </Helmet>
+      <div>search</div>
+    </div>
+  );
 };
