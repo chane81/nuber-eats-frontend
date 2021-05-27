c34f564e7f29ced00cd06e7f032e6421ad9741f3
BM <chane81@naver.com>
2021-05-27 19:05:02 +0900

    Link 삽입

--- src/components/restaurant.tsx
+++ src/components/restaurant.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 interface IProps {
   id: string;
   coverImg: string;
@@ -5,17 +7,24 @@ interface IProps {
   categoryName?: string;
 }
 
-export const Restaurant: React.FC<IProps> = ({ coverImg, name, categoryName }) => {
+export const Restaurant: React.FC<IProps> = ({
+  id,
+  coverImg,
+  name,
+  categoryName,
+}) => {
   return (
-    <div className='flex flex-col'>
-      <div
-        style={{ backgroundImage: `url(${coverImg})` }}
-        className='py-28 bg-cover bg-center mb-3'
-      />
-      <h3 className='text-xl'>{name}</h3>
-      <span className='border-t mt-1 py-2 text-xs opacity-50 border-gray-400'>
-        {categoryName}
-      </span>
-    </div>
+    <Link to={`/restaurants/${id}`}>
+      <div className='flex flex-col'>
+        <div
+          style={{ backgroundImage: `url(${coverImg})` }}
+          className='py-28 bg-cover bg-center mb-3'
+        />
+        <h3 className='text-xl'>{name}</h3>
+        <span className='border-t mt-1 py-2 text-xs opacity-50 border-gray-400'>
+          {categoryName}
+        </span>
+      </div>
+    </Link>
   );
 };
