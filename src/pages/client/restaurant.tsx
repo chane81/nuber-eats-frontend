955e411267c118d66c161e664b4652fc38b79ecd
chane81 <chane81@naver.com>
2021-05-30 03:07:23 +0900

    스타일 수정

--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -2,7 +2,6 @@ import { gql, useQuery } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 import { RESTAURANT_FRAGMENT } from '../../fragments';
 import { restaurant, restaurantVariables } from '../../__generated__/restaurant';
-import { Restaurant as Rest } from '../../components/restaurant';
 
 const RESTAURANT_QUERY = gql`
   query restaurant($input: RestaurantInput!) {
@@ -35,7 +34,24 @@ export const Restaurant = () => {
     },
   );
 
-  console.log('pa', data);
-
-  return <h1>restaurant</h1>;
+  return (
+    <div>
+      <div
+        className='bg-gray-800 bg-center bg-cover py-32 md:py-44'
+        style={{
+          backgroundImage: `url(${data?.restaurant.restaurant?.coverImg})`,
+        }}
+      >
+        <div className='bg-white w-96 md:w-128 py-8 pl-10'>
+          <h4 className='text-4xl mb-3'>{data?.restaurant.restaurant?.name}</h4>
+          <h5 className='text-sm font-light mb-2'>
+            {data?.restaurant.restaurant?.category?.name}
+          </h5>
+          <h6 className='text-sm font-light'>
+            {data?.restaurant.restaurant?.address}
+          </h6>
+        </div>
+      </div>
+    </div>
+  );
 };
