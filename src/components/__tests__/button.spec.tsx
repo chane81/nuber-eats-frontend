76f7768e68ba6e1ac169d3046715107d513c8f2d
chane81 <chane81@naver.com>
2021-05-31 02:54:26 +0900

    button test 추가

---
+++ src/components/__tests__/button.spec.tsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+import { Button } from '../button';
+
+describe('<Button />', () => {
+  it('should render ok with props', () => {
+    const { getByText } = render(
+      <Button canClick={true} loading={false} actionText='test'></Button>,
+    );
+
+    getByText('test');
+  });
+
+  it('should display loading', () => {
+    const { getByText, container } = render(
+      <Button canClick={false} loading={true} actionText='test'></Button>,
+    );
+
+    getByText('Loading...');
+    expect(container.firstChild).toHaveClass('bg-gray-300 pointer-events-none');
+  });
+});
