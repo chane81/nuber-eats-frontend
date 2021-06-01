fd9f054d6e927fd2765fe9318dbb55913e09ca2a
BM <chane81@naver.com>
2021-06-01 19:48:12 +0900

    form-error, header, restaurant 컴포넌트 테스팅 추가

---
+++ src/components/__tests__/header.spec.tsx
@@ -0,0 +1,73 @@
+import { getByText, queryByText, render, waitFor } from '@testing-library/react';
+import { MockedProvider } from '@apollo/client/testing';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Header } from '../header';
+import { ME_QUERY } from '../../hooks/useMe';
+
+describe('<Header />', () => {
+  it('renders ok', async () => {
+    await waitFor(async () => {
+      const { queryByText } = render(
+        <MockedProvider
+          mocks={[
+            {
+              request: {
+                query: ME_QUERY,
+              },
+              result: {
+                data: {
+                  me: {
+                    id: 1,
+                    email: '',
+                    role: '',
+                    verified: true,
+                  },
+                },
+              },
+            },
+          ]}
+        >
+          <Router>
+            <Header />
+          </Router>
+        </MockedProvider>,
+      );
+
+      await new Promise((resolve) => setTimeout(resolve, 0));
+      expect(queryByText('Prease verify your email.')).toBeNull();
+    });
+  });
+
+  it('renders without verify banner', async () => {
+    await waitFor(async () => {
+      const { getByText } = render(
+        <MockedProvider
+          mocks={[
+            {
+              request: {
+                query: ME_QUERY,
+              },
+              result: {
+                data: {
+                  me: {
+                    id: 1,
+                    email: '',
+                    role: '',
+                    verified: false,
+                  },
+                },
+              },
+            },
+          ]}
+        >
+          <Router>
+            <Header />
+          </Router>
+        </MockedProvider>,
+      );
+
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      getByText('Prease verify your email.');
+    });
+  });
+});
---
+++ src/components/__tests__/restaurant.spec.tsx
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/react';
+import { debug } from 'node:console';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Restaurant } from '../restaurant';
+
+describe('<Restaurant />', () => {
+  it('renders ok with props', () => {
+    const restaurantProps = {
+      id: '1',
+      name: 'name',
+      coverImg: 'cov',
+      categoryName: 'catName',
+    };
+
+    const { debug, getByText, container } = render(
+      <Router>
+        <Restaurant {...restaurantProps} />,
+      </Router>,
+    );
+
+    getByText('name');
+    getByText('catName');
+    expect(container.getElementsByClassName('bg-cover')[0]).toHaveAttribute(
+      'style',
+      'background-image: url(cov);',
+    );
+    expect(container.firstChild).toHaveAttribute('href', '/restaurant/1');
+  });
+});
