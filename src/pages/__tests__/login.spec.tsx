bb69f75e4bb6ecdcd52ab12d7e60134c675d9770
BM <chane81@naver.com>
2021-06-02 17:55:10 +0900

    login test 추가

--- src/pages/__tests__/login.spec.tsx
+++ src/pages/__tests__/login.spec.tsx
@@ -1,16 +1,21 @@
 import { ApolloProvider } from '@apollo/client';
-import { createMockClient } from 'mock-apollo-client';
-import { render, waitFor } from '@testing-library/react';
-import { Login } from '../login';
+import { createMockClient, MockApolloClient } from 'mock-apollo-client';
+import { render, RenderResult, waitFor } from '@testing-library/react';
+import { Login, LOGIN_MUTATION } from '../login';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter as Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { loginMutation } from '../../__generated__/loginMutation';
 
 describe('<Login />', () => {
+  let renderResult: RenderResult;
+  let mockedClient: MockApolloClient;
+
   beforeEach(async () => {
     await waitFor(() => {
-      const mockedClient = createMockClient();
+      mockedClient = createMockClient();
 
-      render(
+      renderResult = render(
         <HelmetProvider>
           <Router>
             <ApolloProvider client={mockedClient}>
@@ -22,11 +27,120 @@ describe('<Login />', () => {
     });
   });
 
+  const loginMutationTest = async (
+    formData: {
+      email: string;
+      password: string;
+    },
+    mutationResult: loginMutation,
+  ) => {
+    const { getByPlaceholderText, debug, getByRole } = renderResult;
+    const email = getByPlaceholderText(/email/i);
+    const password = getByPlaceholderText(/password/i);
+    const submitBtn = getByRole('button');
+
+    const mockedMutationResponse = jest.fn().mockResolvedValue({
+      data: {
+        ...mutationResult,
+      },
+    });
+
+    mockedClient.setRequestHandler(LOGIN_MUTATION, mockedMutationResponse);
+
+    await waitFor(() => {
+      userEvent.type(email, formData.email);
+      userEvent.type(password, formData.password);
+      userEvent.click(submitBtn);
+    });
+
+    expect(mockedMutationResponse).toHaveBeenCalledTimes(1);
+    expect(mockedMutationResponse).toHaveBeenCalledWith({
+      loginInput: {
+        ...formData,
+      },
+    });
+
+    return {
+      emailCtrl: email,
+      passwordCtrl: password,
+      submitBtn,
+      getByRole,
+      debug,
+      mockedMutationResponse,
+    };
+  };
+
   it('should render ok', async () => {
     await waitFor(() => {
       expect(document.title).toBe('Login | Nuber Eats');
     });
   });
 
-  it('display email validation errors', async () => {});
+  it('display email validation errors', async () => {
+    const { getByPlaceholderText, debug, getByRole } = renderResult;
+    const email = getByPlaceholderText(/email/i);
+
+    await waitFor(() => {
+      userEvent.type(email, 'test@mail');
+    });
+
+    let errorMessage = getByRole('alert');
+    expect(errorMessage).toHaveTextContent('Please enter a valid email');
+
+    await waitFor(() => {
+      userEvent.clear(email);
+    });
+
+    errorMessage = getByRole('alert');
+    expect(errorMessage).toHaveTextContent(/email is required/i);
+  });
+
+  it('display password required errors', async () => {
+    const { getByPlaceholderText, debug, getByRole } = renderResult;
+    const email = getByPlaceholderText(/email/i);
+    const submitBtn = getByRole('button');
+
+    await waitFor(() => {
+      userEvent.type(email, 'test@mail.com');
+      userEvent.click(submitBtn);
+    });
+
+    const errorMessage = getByRole('alert');
+    expect(errorMessage).toHaveTextContent(/password is required/i);
+  });
+
+  it('submit form and calls mutation', async () => {
+    const formData = {
+      email: 'test@test.com',
+      password: '1234',
+    };
+
+    await loginMutationTest(formData, {
+      login: {
+        ok: true,
+        token: '123ff',
+        error: null,
+        __typename: 'LoginOutput',
+      },
+    });
+  });
+
+  it('submit form and calls mutation error', async () => {
+    const formData = {
+      email: 'test@test.com',
+      password: '1234',
+    };
+
+    const { getByRole } = await loginMutationTest(formData, {
+      login: {
+        ok: false,
+        token: null,
+        error: 'mutation error',
+        __typename: 'LoginOutput',
+      },
+    });
+
+    const errorMessage = getByRole('alert');
+    expect(errorMessage).toHaveTextContent(/mutation error/i);
+  });
 });
