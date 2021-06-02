ba59fb7b8d79a005489988668c18e8ae63a065df
BM <chane81@naver.com>
2021-06-02 17:53:46 +0900

    role='button' 추가

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -6,8 +6,11 @@ interface IProps {
 
 export const Button: React.FC<IProps> = ({ canClick, loading, actionText }) => (
   <button
+    role='button'
     className={`text-lg font-medium focus:outline-none text-white py-4 transition-colors ${
-      canClick ? 'bg-lime-600 hover:bg-lime-700' : 'bg-gray-300 pointer-events-none'
+      canClick
+        ? 'bg-lime-600 hover:bg-lime-700'
+        : 'bg-gray-300 pointer-events-none'
     }`}
   >
     {loading ? 'Loading...' : actionText}
