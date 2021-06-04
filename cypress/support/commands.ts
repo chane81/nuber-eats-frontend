aed1f6a7762e8d01a5fc9fea7f72ddd6ae8bab99
BM <chane81@naver.com>
2021-06-04 16:46:08 +0900

    커스텀 커맨드 코드 정리

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -20,15 +20,48 @@
 declare global {
   namespace Cypress {
     interface Chainable {
-      // 로그인 체크
+      // 로그인 토큰 체크
       assertLoggedIn(): Chainable<string>;
+
+      // 로그아웃 체크
+      assertLoggedOut(): Chainable<string>;
+
+      // 로그인
+      login(email: string, password: string): void;
     }
   }
 }
 
 import '@testing-library/cypress/add-commands';
 
-// 로그인 체크
+// 로그인 토큰 체크
 Cypress.Commands.add('assertLoggedIn', () => {
   cy.window().its('localStorage.nuber-token').should('be.a', 'string');
+});
+
+// 로그아웃 체크
+Cypress.Commands.add('assertLoggedOut', () => {
+  cy.window().its('localStorage.nuber-token').should('be.undefined');
+});
+
+// 로그인
+Cypress.Commands.add('login', (email, password) => {
+  // visit
+  cy.visit('/');
+
+  // 로그아웃 상태 체크
+  cy.assertLoggedOut();
+
+  // 로그인페이지 진입 확인
+  cy.title().should('eq', 'Login | Nuber Eats');
+
+  // 이메일/패스워드 입력
+  cy.findByPlaceholderText(/email/i).type(email);
+  cy.findByPlaceholderText(/password/i).type(password);
+
+  // 버튼의 pointer-events-none class 가 없는지 체크 후 click 발생
+  cy.findByRole('button').should('not.have.class', 'pointer-events-none').click();
+
+  // local storage 에 토큰있는지 체크
+  cy.assertLoggedIn();
 });
