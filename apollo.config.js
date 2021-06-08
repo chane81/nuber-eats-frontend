8266c652607e46e6ab5da2e96de1e84e6c5906f3
BM <chane81@naver.com>
2021-06-08 18:06:31 +0900

    excludes 옵션 추가

--- apollo.config.js
+++ apollo.config.js
@@ -2,6 +2,7 @@ module.exports = {
   client: {
     // tsx | ts 인 확장자인 파일에서 query 를 검색
     includes: ['./src/**/*.{ts,tsx}'],
+    excludes: ['./**/*.spec.ts', './node_modules'],
     // tag 명
     tagName: 'gql',
     // graphql backend setting
