dbbe84eb1595b75741eb9bdc0a556ed925f7d6cd
chane81 <chane81@naver.com>
2021-06-15 23:57:29 +0900

    Dish 컴포넌트 적용

--- src/pages/owner/my-restaurant.tsx
+++ src/pages/owner/my-restaurant.tsx
@@ -1,7 +1,7 @@
 import { gql, useQuery } from '@apollo/client';
 import { Helmet } from 'react-helmet-async';
-import { useParams } from 'react-router';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { Dish } from '../../components/dish';
 import { DISH_FRAGMENT, RESTAURANT_FRAGMENT } from '../../fragments';
 import {
   myRestaurant,
@@ -52,11 +52,11 @@ export const MyRestaurant = () => {
         </title>
       </Helmet>
       <div
-        className='bg-gray-700 py-28 bg-center bg-cover'
+        className='bg-gray-700 py-20 md:py-28 bg-center bg-cover'
         style={{
           backgroundImage: `url(${data?.myRestaurant.restaurant?.coverImg})`,
         }}
-      ></div>
+      />
       <div className='container mt-10'>
         <h2 className='text-4xl font-medium mb-10'>
           {data?.myRestaurant.restaurant?.name || 'Loading...'}
@@ -67,13 +67,24 @@ export const MyRestaurant = () => {
         >
           Add Dish &rarr;
         </Link>
-        <Link to={''} className='text-white bg-lime-700 py-3 px-10'>
+        <Link to='' className='text-white bg-lime-700 py-3 px-10'>
           Buy Promotion &rarr;
         </Link>
         <div className='mt-10'>
           {data?.myRestaurant.restaurant?.menu.length === 0 ? (
             <h4 className='text-xl mb-5'>Please upload a dish!</h4>
-          ) : null}
+          ) : (
+            <div className='grid mt-16 px-3 md:grid-cols-3 gap-x-5 gap-y-10'>
+              {data?.myRestaurant.restaurant?.menu.map((dish) => (
+                <Dish
+                  key={dish.id}
+                  name={dish.name}
+                  description={dish.description}
+                  price={dish.price}
+                />
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </div>
