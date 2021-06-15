28b4c341f410159e0c02bdbef3022ec8c41f2774
chane81 <chane81@naver.com>
2021-06-15 23:57:20 +0900

    dish 메뉴 컴포넌트 commit

--- src/components/dish.tsx
+++ src/components/dish.tsx
@@ -4,6 +4,14 @@ interface IProps {
   price: number;
 }
 
-export const Dish = () => {
-  return <span></span>;
+export const Dish: React.FC<IProps> = ({ description, name, price }) => {
+  return (
+    <div className='px-8 pt-3 pb-8 border hover:border-gray-500 transition-all'>
+      <div className='mb-5'>
+        <h3 className='text-lg font-medium'>{name}</h3>
+        <h4 className='font-medium'>{description}</h4>
+      </div>
+      <span>${price}</span>
+    </div>
+  );
 };
