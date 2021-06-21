554ce77863c1529cbe92159d1a712cd5fbdcb939
chane81 <chane81@naver.com>
2021-06-21 21:01:23 +0900

    dish 컴포넌트에 props 추가

--- src/components/dish.tsx
+++ src/components/dish.tsx
@@ -1,22 +1,49 @@
 import { restaurant_restaurant_restaurant_menu_options } from '../__generated__/restaurant';
 
 interface IProps {
+  id?: number;
   description: string;
   name: string;
   price: number;
   isCustomer?: boolean;
+  orderStarted?: boolean;
+  isSelected?: boolean;
   options?: restaurant_restaurant_restaurant_menu_options[] | null;
+  addItemToOrder?: (dishId: number) => void;
+  removeFromOrder?: (dishId: number) => void;
 }
 
 export const Dish: React.FC<IProps> = ({
+  id = 0,
   description,
   name,
   price,
   isCustomer = false,
+  isSelected = false,
+  orderStarted = false,
   options,
+  addItemToOrder,
+  removeFromOrder,
 }) => {
+  const handleClick = () => {
+    if (orderStarted) {
+      if (!isSelected) {
+        addItemToOrder?.(id);
+      }
+
+      if (isSelected) {
+        removeFromOrder?.(id);
+      }
+    }
+  };
+
   return (
-    <div className='px-8 pt-3 pb-8 border hover:border-gray-500 transition-all'>
+    <div
+      onClick={handleClick}
+      className={`px-8 pt-3 pb-8 border cursor-pointer transition-all ${
+        isSelected ? 'border-lime-600 border-b-4' : 'hover:border-gray-800 '
+      }`}
+    >
       <div className='mb-5'>
         <h3 className='text-lg font-medium'>{name}</h3>
         <h4 className='font-medium'>{description}</h4>
