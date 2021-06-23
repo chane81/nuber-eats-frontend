ca8301a351a7f00005bdafd99f2d9718b90252ae
chane81 <chane81@naver.com>
2021-06-24 01:53:49 +0900

    신규 생성

---
+++ src/pages/order.tsx
@@ -0,0 +1,45 @@
+import { gql, useQuery } from '@apollo/client';
+import { useParams } from 'react-router-dom';
+import { getOrder, getOrderVariables } from '../__generated__/getOrder';
+
+const GET_ORDER = gql`
+  query getOrder($input: GetOrderInput!) {
+    getOrder(input: $input) {
+      ok
+      error
+      order {
+        id
+        status
+        total
+        driver {
+          email
+        }
+        customer {
+          email
+        }
+        restaurant {
+          name
+        }
+      }
+    }
+  }
+`;
+
+interface IParams {
+  id: string;
+}
+
+export const Order = () => {
+  const params = useParams<IParams>();
+  const { data } = useQuery<getOrder, getOrderVariables>(GET_ORDER, {
+    variables: {
+      input: {
+        id: +params.id,
+      },
+    },
+  });
+
+  console.log('data', data);
+
+  return <h1>{params.id}</h1>;
+};
