684144a12d7bf8e99bae6117c97df1fe9923835a
chane81 <chane81@naver.com>
2021-06-27 23:06:28 +0900

    로그아웃 버튼및 기능 추가

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,21 @@
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faSignOutAlt, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { authTokenVar, isLoggedInVar } from '../apollo';
+import { LOCALSTORAGE_TOKEN } from '../constants';
 import { useMe } from '../hooks/useMe';
 import nuberLogo from '../images/logo.svg';
 
 export const Header: React.FC = () => {
   const { data } = useMe();
+  const history = useHistory();
+
+  const handleLogout = () => {
+    localStorage.setItem(LOCALSTORAGE_TOKEN, '');
+    authTokenVar('');
+    isLoggedInVar(false);
+    history.replace('/');
+  };
 
   return (
     <>
@@ -14,16 +24,32 @@ export const Header: React.FC = () => {
           <span>Prease verify your email.</span>
         </div>
       )}
-      <header className='py-4'>
-        <div className='w-full max-w-screen-xl mx-auto flex justify-between items-center'>
+      <header className='py-4 fixed top-0 bg-white w-full z-10'>
+        <div className='container mx-auto flex justify-between items-center'>
           <Link title='home' to='/'>
             <img src={nuberLogo} className='w-36' alt='logo' />
           </Link>
-          <span className='text-xs'>
-            <Link title='edit-profile' to='/edit-profile'>
-              <FontAwesomeIcon icon={faUser} className='text-xl' />
-            </Link>
-          </span>
+          <div className='flex w-52 justify-between'>
+            <div className='text-xs flex items-center'>
+              <Link className='mr-2' title='edit-profile' to='/edit-profile'>
+                <FontAwesomeIcon
+                  icon={faUser}
+                  className='text-xl mr-2 text-gray-600'
+                />
+                <span className='font-medium text-gray-500'>Edit Profile</span>
+              </Link>
+            </div>
+            <div
+              onClick={handleLogout}
+              className='text-xs cursor-pointer flex items-center'
+            >
+              <FontAwesomeIcon
+                icon={faSignOutAlt}
+                className='text-xl text-gray-600 mr-2'
+              />
+              <span className='font-medium text-gray-500'>Sign Out</span>
+            </div>
+          </div>
         </div>
       </header>
     </>
