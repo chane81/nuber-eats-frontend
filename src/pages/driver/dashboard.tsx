0983db71113a3779d02464453bf837b88f0c5fb4
chane81 <chane81@naver.com>
2021-06-28 01:08:21 +0900

    location 관련 로직 추가

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,19 +1,37 @@
 import GoogleMapReact from 'google-map-react';
+import { useEffect, useState } from 'react';
+
+interface ICoords {
+  lat: number;
+  lng: number;
+}
 
 export const Dashboard = () => {
+  const [driverCoords, setDriverCoords] = useState<ICoords>({ lat: 0, lng: 0 });
+  const onSuccess = ({ coords: { latitude, longitude } }: GeolocationPosition) => {
+    setDriverCoords({ lat: latitude, lng: longitude });
+  };
+
+  const onError = (position: GeolocationPositionError) => {
+    console.log(position);
+  };
+  useEffect(() => {
+    navigator.geolocation.watchPosition(onSuccess, onError, {
+      enableHighAccuracy: true,
+    });
+  }, []);
+
   return (
     <div>
       <div className='' style={{ width: window.innerWidth, height: '95vh' }}>
         <GoogleMapReact
-          defaultZoom={20}
+          defaultZoom={15}
           defaultCenter={{
-            lat: 59.95,
-            lng: 30.33,
+            lat: 37.63758368597268,
+            lng: 126.78774824975517,
           }}
           bootstrapURLKeys={{ key: 'AIzaSyDsK3fRWCi19RvDRYKAqY-haTqridS0jOw' }}
-        >
-          <h1>Hello</h1>
-        </GoogleMapReact>
+        />
       </div>
     </div>
   );
