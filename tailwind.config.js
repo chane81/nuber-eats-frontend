b248e976c6512dfde8e10cd5e0d85b219f731a3b
chane81 <chane81@naver.com>
2021-07-19 00:49:23 +0900

    add puge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: [],
+  // NODE_ENV=production 모드에서
+  // tsx 파일에서 쓰인 클래스만 build
+  purge: ['./src/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
