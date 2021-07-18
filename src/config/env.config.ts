ed440508f8e037c4fe6e09c694f6ba3a3242e3a5
chane81 <chane81@naver.com>
2021-07-19 01:07:04 +0900

    add graphql env

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,6 +1,8 @@
 // 환경 변수
 const envVars = {
   GOOGLE_MAP_KEY: process.env.REACT_APP_GOOGLE_MAP_KEY || '',
+  GRAPHQL_URL: process.env.REACT_APP_GRAPHQL_URL || '',
+  GRAPHQL_WS: process.env.REACT_APP_GRAPHQL_WS || '',
 };
 
 export { envVars };
